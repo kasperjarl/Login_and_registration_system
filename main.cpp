e767e66a7c55ef8e52593bb297d12ec21129e679
kasperjarl <kasper.jarlgren@gmail.com>
2025-09-18 19:57:27 +0200

    Project plan

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int main()
+{
+	/*
+	
+	0. Ask the user if they want to login or registrate 
+
+	1. Login part
+		1.1 Ask for username
+		1.2 Ask for password (would be nice if it showed '*' instead of letters)
+		1.3 Check the file for a login that mathes
+		1.4 Give a succes/failure message
+		1.5 Give the user an option to logut
+
+	
+	
+	2. Registration part
+		2.1 Ask for username
+		2.2 If username avaiable proceed
+		2.3 Ask for password (would be nice if it showed '*' instead of letters)
+		2.4 Ask User to confirm password
+		2.5 If Password matches proceed
+		2.6 Store user
+		2.7 Ask user to login
+	
+	Classes:
+		- User (username, password, login attemps?)
+		- Login/registration page (login/registration, check if user in file, 
+			handle invalid login).
+	
+	*/
+
+
+	return 0;
+}
